9190a5c0a18e2396bae6decbb4cf75616a27ea4a
Ashutosh Rai <mahadevashu182@gmail.com>
2024-04-19 21:25:50 +0530

    modified:   03-Terraform-Settings-Providers-Resources/terraform-manifests/app1-install.sh modified:   03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,8 +1,8 @@
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
-  instance_type = "t3.micro"
-  user_data = file("${path.module}/app1-install.sh") #variable expansion
+  ami = "ami-007020fd9c84e18c7"
+  instance_type = "t2.micro"
+  user_data = file("${path.module}/app1-install.sh") #${}variable expansion
   tags = {
     "Name" = "EC2 Demo"
   }
