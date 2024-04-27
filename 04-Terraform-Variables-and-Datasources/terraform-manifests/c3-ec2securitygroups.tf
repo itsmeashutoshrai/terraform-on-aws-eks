d998cdbaa621ce21e9952653cb5462e5f689bc78
Ashutosh Rai <mahadevashu182@gmail.com>
2024-04-27 14:12:43 +0530

    modified:   04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
@@ -37,24 +37,8 @@ resource "aws_vpc_security_group_ingress_rule" "vpc-ssh_ipv4_web" {
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_another" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_web" {
   security_group_id = aws_security_group.vpc-ssh.web.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
-
-/*
-resource "aws_vpc_security_group_ingress_rule" "vpc-ssh_ipv6" {
-  security_group_id = aws_security_group.vpc-ssh.id
-  cidr_ipv6         = aws_vpc.main.ipv6_cidr_block
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.vpc-ssh.id
-  cidr_ipv6         = "::/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
-*/
