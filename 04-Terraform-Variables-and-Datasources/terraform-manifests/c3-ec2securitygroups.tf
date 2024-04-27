b1a54c15c25bfed0cd0df26434d2afd6bf95305c
Ashutosh Rai <mahadevashu182@gmail.com>
2024-04-27 14:07:12 +0530

    modified:   04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
@@ -37,7 +37,7 @@ resource "aws_vpc_security_group_ingress_rule" "vpc-ssh_ipv4_web" {
   to_port           = 443
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_another" {
   security_group_id = aws_security_group.vpc-ssh.web.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
