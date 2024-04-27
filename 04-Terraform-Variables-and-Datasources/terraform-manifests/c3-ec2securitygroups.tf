8fcaa27cf2c10791de3cc7e30ee8c234b5ea7320
Ashutosh Rai <mahadevashu182@gmail.com>
2024-04-27 16:57:11 +0530

    modified:   04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c3-ec2securitygroups.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "vpc-ssh" {
+resource "aws_security_group" "vpc_ssh" {
   name        = "vpc-ssh"
   description = "Allow vpc-ssh traffic"
   vpc_id      = aws_vpc.main.id
@@ -7,8 +7,8 @@ resource "aws_security_group" "vpc-ssh" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "vpc-ssh_ipv4" {
-  security_group_id = aws_security_group.vpc-ssh.id
+resource "aws_vpc_security_group_ingress_rule" "vpc_ssh_ipv4" {
+  security_group_id = aws_security_group.vpc_ssh.id
   cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 22
   ip_protocol       = "tcp"
@@ -16,12 +16,12 @@ resource "aws_vpc_security_group_ingress_rule" "vpc-ssh_ipv4" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.vpc-ssh.id
+  security_group_id = aws_security_group.vpc_ssh.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-resource "aws_security_group" "vpc-ssh-web" {
+resource "aws_security_group" "vpc_ssh_web" {
   name        = "vpc-ssh-web"
   description = "Allow vpc-ssh traffic"
   vpc_id      = aws_vpc.main.id
@@ -29,8 +29,9 @@ resource "aws_security_group" "vpc-ssh-web" {
     Name = "vpc-ssh-web"
   }
 }
-resource "aws_vpc_security_group_ingress_rule" "vpc-ssh_ipv4_web" {
-  security_group_id = aws_security_group.vpc-ssh-web.id
+
+resource "aws_vpc_security_group_ingress_rule" "vpc_ssh_ipv4_web" {
+  security_group_id = aws_security_group.vpc_ssh_web.id
   cidr_ipv4         = aws_vpc.main.cidr_block
   from_port         = 443
   ip_protocol       = "tcp"
@@ -38,7 +39,7 @@ resource "aws_vpc_security_group_ingress_rule" "vpc-ssh_ipv4_web" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4_web" {
-  security_group_id = aws_security_group.vpc-ssh-web.id
+  security_group_id = aws_security_group.vpc_ssh_web.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
