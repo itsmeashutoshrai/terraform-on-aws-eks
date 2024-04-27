aba32237f001db16223b847ebbc6b66265d28955
Ashutosh Rai <mahadevashu182@gmail.com>
2024-04-27 13:17:38 +0530

    modified:   03-Terraform-Settings-Providers-Resources/terraform-manifests/test.tf 	modified:   04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf modified:   04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf

--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
@@ -1,10 +1,10 @@
 # Get latest AMI ID for Amazon Linux2 OS
-data "aws_ami" "amzlinux2" {
+data "aws_ami" "ubuntu_free_tier" {
   most_recent      = true
   owners           = ["amazon"]
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-gp2"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240423"]
   }
   filter {
     name   = "root-device-type"
