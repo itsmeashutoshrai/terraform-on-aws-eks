68a26db875dcd0a723347e7a7e7ef1736715b6b1
Ashutosh Rai <mahadevashu182@gmail.com>
2024-05-04 16:03:51 +0530

    modified:   03-Terraform-Settings-Providers-Resources/terraform-manifests/test.tf 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/c4-az-datasoure.tf 	modified:   04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf.bkp

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/test.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/test.tf
@@ -1,2 +1,15 @@
-#output test
-output "instance_publicip"
+from pytube import YouTube
+
+def download_video(url, output_directory):
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.get_highest_resolution()
+        stream.download(output_path=output_directory)
+        print("Download completed successfully!")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+# Example usage
+video_url = "https://www.youtube.com/watch?v=n06H7OcPd-g"
+download_directory = "/path/to/download/directory"
+download_video(video_url, download_directory)
--- 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/c5-ec2instance.tf
@@ -1,10 +1,12 @@
-# EC2 Instance
+# Adding the availabilty zone and fetching the datasource value for az "ap-south-1a" by default it was craetaing an instance at 
+#ap-south-1b but changed here to az "ap-south-1a".
 resource "aws_instance" "myec2vm" {
-  ami = data.aws_ami.ubuntu_free_tier.id
-  instance_type = var.instance_type
-  user_data = file("${path.module}/app1-install.sh")
-  key_name = var.instance_keypair
-  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
+  ami                    = data.aws_ami.ubuntu_free_tier.id
+  instance_type          = var.instance_type
+  user_data              = file("${path.module}/app1-install.sh")
+  key_name               = var.instance_keypair
+  availability_zone = data.aws_availability_zones.myaz.names[0]
+  vpc_security_group_ids = [aws_security_group.vpc-sshh.id, aws_security_group.vpc-webb.id]
   tags = {
     "Name" = "EC2 Demo 2"
   }
