6d15ea653ff5abc968342cb03b8d6ac9c7088e89
Ashutosh Rai <mahadevashu182@gmail.com>
2024-05-13 15:58:24 +0530

    new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/app1-install.sh 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c1-versions.tf 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c2-variables.tf 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c3-ec2securitygroups.tf 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c3-ec2securitygroups.tf.bkp 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c4-ami-datasource.tf 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c4-az-datasoure.tf 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c5-ec2instance.tf 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c5-ec2instance.tf.bkp 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c6-outputs.tf 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/private-key/terraform-key.pem 	new file:   04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/private-key/test-key-pair.pem

---
+++ 04-Terraform-Variables-and-Datasources/terraform-manifests/terraform-manifests/c4-ami-datasource.tf
@@ -0,0 +1,27 @@
+# Get latest AMI ID for Amazon Linux2 OS
+data "aws_ami" "ubuntu_free_tier" {
+  most_recent      = true
+  owners           = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240423"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+/*
+# datasource for default vpc to fetch the default vpc id in c6-output.tf file and used in ec2instance.tf #file
+data "aws_vpc" "selected" {
+  default = true
+}
+*/
